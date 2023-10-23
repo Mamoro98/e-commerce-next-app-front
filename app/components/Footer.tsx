79e7dfbd35bcb83b52ccaa1493c5c2df466db732
Omer Mamaoro <endomamoro9@gmail.com>
2023-10-24 00:14:11 +0300

    change link of flowbit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -87,7 +87,7 @@ const Footer = () => {
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023{" "}
-              <a href="https://flowbite.com/" className="hover:underline">
+              <a href="https://morosama.vercel.app" className="hover:underline">
                 Mamoro™
               </a>
               . All Rights Reserved.
